3193da0ed57b26de144614fc28c9f0ee4ba65e52
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-21 20:25:23 -0300

    fix ProductRepository

--- src/main/java/com/cleanArchitectureAndSolidstudy/api/domain/repositories/ProductRepository.java
+++ src/main/java/com/cleanArchitectureAndSolidstudy/api/domain/repositories/ProductRepository.java
@@ -3,11 +3,12 @@ package com.cleanArchitectureAndSolidstudy.api.domain.repositories;
 import com.cleanArchitectureAndSolidstudy.api.domain.entities.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository {
     Product save(Product product);
     List<Product> findAll();
-    Product findById(Long id);
+    Optional<Product> findById(Long id);
     void deleteById(Long id);
     List<Product> findByName(String name);
     List<Product> findByPriceGreaterThan(Double price);
