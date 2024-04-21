2321b501a47539ec5d295071dbbbdbacd99154d8
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-21 20:26:38 -0300

    fix CategoryRepository

--- src/main/java/com/cleanArchitectureAndSolidstudy/api/domain/repositories/CategoryRepository.java
+++ src/main/java/com/cleanArchitectureAndSolidstudy/api/domain/repositories/CategoryRepository.java
@@ -3,11 +3,12 @@ package com.cleanArchitectureAndSolidstudy.api.domain.repositories;
 import com.cleanArchitectureAndSolidstudy.api.domain.entities.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository {
     Category save(Category category);
     List<Category> findAll();
-    Category findById(Long id);
+    Optional<Category> findById(Long id);
     void deleteById(Long id);
     List<Category> findByName(String name);
 }
