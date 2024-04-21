0c5431b6f128ed4c4c1df5f9c5c8fec36318f4dd
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-21 19:47:14 -0300

    Create entities

---
+++ src/main/java/com/cleanArchitectureAndSolidstudy/api/domain/entities/Category.java
@@ -0,0 +1,23 @@
+package com.cleanArchitectureAndSolidstudy.api.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
