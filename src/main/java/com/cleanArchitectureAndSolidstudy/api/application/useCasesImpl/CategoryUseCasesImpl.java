ad46346c74a508a88cd72b66b7516b31cf6f03c6
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-21 22:54:07 -0300

    Implement Save CategoryUseCase

--- src/main/java/com/cleanArchitectureAndSolidstudy/api/application/useCasesImpl/CategoryUseCasesImpl.java
+++ src/main/java/com/cleanArchitectureAndSolidstudy/api/application/useCasesImpl/CategoryUseCasesImpl.java
@@ -1,14 +1,24 @@
 package com.cleanArchitectureAndSolidstudy.api.application.useCasesImpl;
 
+import com.cleanArchitectureAndSolidstudy.api.application.useCases.CategoryUseCases;
+import com.cleanArchitectureAndSolidstudy.api.domain.entities.Category;
 import com.cleanArchitectureAndSolidstudy.api.domain.repositories.CategoryRepository;
+import com.cleanArchitectureAndSolidstudy.api.infra.dtos.CategoryDto;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CategoryUseCasesImpl {
+public class CategoryUseCasesImpl implements CategoryUseCases {
     private final CategoryRepository categoryRepository;
 
     public CategoryUseCasesImpl(CategoryRepository categoryRepository){
         this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public CategoryDto save(CategoryDto categoryDto){
+        Category category = new Category(categoryDto);
+        Category savedCategory = categoryRepository.save(category);
+        return CategoryDto.fromCategory(savedCategory);
     }
 
     
