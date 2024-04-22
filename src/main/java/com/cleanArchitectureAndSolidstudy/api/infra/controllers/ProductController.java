77a2ebdf428a8ff21a6e93ca1816eb214d3e4c62
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-21 23:18:04 -0300

    Update ProductController.java

--- src/main/java/com/cleanArchitectureAndSolidstudy/api/infra/controllers/ProductController.java
+++ src/main/java/com/cleanArchitectureAndSolidstudy/api/infra/controllers/ProductController.java
@@ -2,10 +2,9 @@ package com.cleanArchitectureAndSolidstudy.api.infra.controllers;
 
 import com.cleanArchitectureAndSolidstudy.api.application.useCases.ProductUseCases;
 import com.cleanArchitectureAndSolidstudy.api.infra.dtos.ProductDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -16,5 +15,48 @@ public class ProductController {
         this.productUseCases = productUseCases;
     }
 
-    
+    @PostMapping
+    public ProductDto create(@RequestBody ProductDto productDto){
+        return productUseCases.save(productDto);
+    }
+
+    @GetMapping
+    public List<ProductDto> findAll() {
+        return productUseCases.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDto findById(@PathVariable Long id) {
+        return productUseCases.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ProductDto update(@PathVariable Long id, @RequestBody ProductDto produtoDto) {
+        ProductDto existingProdutoDto = productUseCases.findById(id);
+        if (existingProdutoDto != null) {
+            existingProdutoDto = produtoDto;
+            return productUseCases.save(existingProdutoDto);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        productUseCases.deleteById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public List<ProductDto> findByNome(@PathVariable String name) {
+        return productUseCases.findByName(name);
+    }
+
+    @GetMapping("/price/{price}")
+    public List<ProductDto> findByPrecoGreaterThan(@PathVariable Double price) {
+        return productUseCases.findByPriceGreaterThan(price);
+    }
+
+    @GetMapping("/category/{name}")
+    public List<ProductDto> findByCategoryName(@PathVariable String name) {
+        return productUseCases.findByCategoryName(name);
+    }
 }
