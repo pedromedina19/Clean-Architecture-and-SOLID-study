da9f6ced3502c92a12a739121e7ed6867154e359
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-21 22:39:07 -0300

    fix DTOs

--- src/main/java/com/cleanArchitectureAndSolidstudy/api/infra/dtos/CategoryDto.java
+++ src/main/java/com/cleanArchitectureAndSolidstudy/api/infra/dtos/CategoryDto.java
@@ -1,4 +1,10 @@
 package com.cleanArchitectureAndSolidstudy.api.infra.dtos;
 
+import com.cleanArchitectureAndSolidstudy.api.domain.entities.Category;
+
 public record CategoryDto(Long id, String name) {
+    // Método que converte Category em CategoryDto
+    public static CategoryDto fromCategory(Category category){
+        return new CategoryDto(category.getId(), category.getName());
+    }
 }
--- src/main/java/com/cleanArchitectureAndSolidstudy/api/infra/dtos/ProductDto.java
+++ src/main/java/com/cleanArchitectureAndSolidstudy/api/infra/dtos/ProductDto.java
@@ -1,4 +1,11 @@
 package com.cleanArchitectureAndSolidstudy.api.infra.dtos;
 
+import com.cleanArchitectureAndSolidstudy.api.domain.entities.Product;
+
 public record ProductDto(Long id, String name, Double price, CategoryDto category) {
+    // Método que converte um Product em ProductDto
+    public static ProductDto fromProduct(Product product){
+        CategoryDto categoryDto = CategoryDto.fromCategory(product.getCategory());
+        return new ProductDto(product.getId(), product.getName(), product.getPrice(), categoryDto);
+    }
 }
